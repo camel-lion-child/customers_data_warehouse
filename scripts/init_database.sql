21ebc916388f4549767e97a7e20795244253c64b
Huyen Tran <127302084+camel-lion-child@users.noreply.github.com>
2026-03-05 16:23:45 +0100

    Add warning about database deletion in init script
    Added a warning comment about potential data loss when dropping the database.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,4 +1,5 @@
 -- Create a new database named datawarehouse. If the database exists, it will be dropped and recreated.
+-- Warning: check the database existed carefully before proceeding! By running theses queries, all the database will be permanently deleted, it is necessarily to have backups.
 DROP DATABASE IF EXISTS datawarehouse;
 CREATE DATABASE datawarehouse;
 
